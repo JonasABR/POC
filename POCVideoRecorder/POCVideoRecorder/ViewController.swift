a2e62729dca66f2630732b07bd92706d8856d4ec
Jonas Alves Simoes <jsimoes@avenuecode.com>
2017-10-31 16:13:53 -0200

    addfiles

---
+++ POCVideoRecorder/POCVideoRecorder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  POCVideoRecorder
+//
+//  Created by Avenue Code on 10/31/17.
+//  Copyright © 2017 AvenueCode. All rights reserved.
+//
+
+import UIKit
+import MediaPlayer
+import MobileCoreServices
+
+class ViewController: UIViewController {
+    @IBOutlet var recordButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
