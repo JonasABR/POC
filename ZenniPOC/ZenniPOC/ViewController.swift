137bffac73d4127c6c78b3f6b1858aa43382bdf5
Jonas Alves Simoes <jsimoes@avenuecode.com>
2017-11-14 11:01:39 -0200

    new project

---
+++ ZenniPOC/ZenniPOC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZenniPOC
+//
+//  Created by Avenue Code on 11/14/17.
+//  Copyright © 2017 AvenueCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
